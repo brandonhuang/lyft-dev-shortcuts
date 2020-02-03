51a652ae7a36e40a233e003d3c0bbb0a86c93670
Brandon Huang <bhuang@lyft.com>
2020-02-03 13:41:14 -0800

    fix bug take 2

--- onebox_set_target.sh
+++ onebox_set_target.sh
@@ -1,7 +1,6 @@
 function upstream() {
     if [ -z "$1" ]; then
-        onebox = `cat $DIR/onebox_target.txt`
-        echo $onebox
+        echo `cat $DIR/onebox_target.txt`
     else
         echo $1 > "$DIR/onebox_target.txt"
         echo "your new onebox target has been set as $1"
